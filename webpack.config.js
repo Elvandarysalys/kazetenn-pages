7f2514da2c9f876b160e07d28ead9870b31b7ae8
Elvandar <gwilherm-alan.turpin@supinfo.com>
2022-01-07 14:18:28 +0100

    add copyright mentions

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Gwilherm-Alan Turpin 2022.
+ *
+ * This file is part of the Kazetenn Pages Bundle
+ *
+ * For more informations about the license, you can refer yourself  to the LICENSE file at the root of the project.
+ */
+
 const Encore = require('@symfony/webpack-encore')
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
