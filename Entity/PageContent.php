da4b0ea2a697b881df695b212a466566b17c7b48
Elvandar <gwilherm-alan.turpin@supinfo.com>
2022-01-07 14:20:23 +0100

    adding more infos in the copyright

--- Entity/PageContent.php
+++ Entity/PageContent.php
@@ -1,10 +1,10 @@
 <?php
 /*
- * Copyright (c) Gwilherm-Alan Turpin 2022.
- *
  * This file is part of the Kazetenn Pages Bundle
  *
- * For more informations about the license, you can refer yourself  to the LICENSE file at the root of the project.
+ * (c) Gwilherm-Alan Turpin (elvandar.ysalys@protonmail.com) 2022.
+ *
+ * For more informations about the license and copyright, please view the LICENSE file at the root of the project.
  */
 
 namespace Kazetenn\Pages\Entity;
